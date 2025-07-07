291ef86629303f30426b2e6625022cc568a01c74
Mr-Nightfall <wilsonwillow477@gmail.com>
2025-07-07 12:42:56 +0800

    modified:   themes/proxio/config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -22,12 +22,12 @@ const CONFIG = {
     PROXIO_HERO_BUTTON_1_URL:'',
         //'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
-    PROXIO_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
+    PROXIO_HERO_BUTTON_2_URL: 'https://github.com/Mr-Nightfall/', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
 
     // 英雄区配图，如需隐藏，改为空值即可 ''
     PROXIO_HERO_BANNER_IMAGE: '', // hero区背景，默认是获取Notion背景，如需另外配置图片可以填写在这里
-    PROXIO_HERO_BANNER_IFRAME_URL: 'https://my.spline.design/untitled-b0c6e886227646c34afc82cdc6de4ca2/', 
+    PROXIO_HERO_BANNER_IFRAME_URL: '', 
 // hero背景区内嵌背景网页 ，可以配置一个网页地址，例如动画网页https://my.spline.design/untitled-b0c6e886227646c34afc82cdc6de4ca2/
 
 
