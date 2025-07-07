5309ed35de8aa93d12ef73d290ff796e9916cdf0
Mr-Nightfall <wilsonwillow477@gmail.com>
2025-07-07 11:14:32 +0800

    modified:   themes/proxio/config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -10,17 +10,17 @@ const CONFIG = {
         '世界给了我一拳，但我出的是布',
         '找个位子随便坐',
         '笔下江山取意成，一峰来尽一峰生',
-        '网安人不说晚安',
+        '网安人别说晚安',
     ], // 欢迎文字，数组形式
 
     // 英雄区块导航
-    PROXIO_HERO_ENABLE: false, // 开启英雄区
-    PROXIO_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
-    PROXIO_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
+    PROXIO_HERO_ENABLE: true, // 开启英雄区
+    PROXIO_HERO_TITLE_1: '', // 英雄区文字
+    PROXIO_HERO_TITLE_2: '', // 英雄区文字
     // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-    PROXIO_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
-    PROXIO_HERO_BUTTON_1_URL:
-        'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
+    PROXIO_HERO_BUTTON_1_TEXT: '', // 英雄区按钮
+    PROXIO_HERO_BUTTON_1_URL:'',
+        //'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
